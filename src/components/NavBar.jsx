23e680002e304961020efa79a2ecd0a4def74417
Bianca Di Biase <Bianca@Biancas-MacBook-Air.local>
2025-01-21 13:20:18 +0800

    made css styling changes

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,47 +1,34 @@
-import { Link, Outlet } from "react-router-dom";
+import React from "react";
+import { Outlet } from "react-router-dom";
 import { useAuth } from "../hooks/use-auth";
-import { useState } from "react";
+import { Link } from "react-router-dom";
 import Image from "../img/logo.svg";
 
 function NavBar() {
-  const { auth, logout } = useAuth();
-  const [menuOpen, setMenuOpen] = useState(false);
-  console.log("NavBar component rendered");
-
-  const toggleMenu = () => {
-    setMenuOpen(!menuOpen);
-  };
-
-  const closeMenu = () => {
-    setMenuOpen(false);
-  };
-
-  const handleLogout = () => {
-    const confirmLogout = window.confirm("Are you sure you want to log out?");
-    if (confirmLogout) {
-      logout(); // Use the logout function from useAuth
-    }
-  };
+  const { auth, setAuth } = useAuth();
 
   return (
-    <div>
-      <nav className="navbar" role="navigation" aria-label="main navigation">
-        <img src={Image} alt="logo" className="logo-image" />
-        <Link to="/">Home</Link>
-        {!auth.token && <Link to="/signup">Sign Up</Link>}{" "}
-        {/*signup will only show if the user is not logged in*/}
-        {auth.token && <Link to="/lists">Lists</Link>}{" "}
-        {/*List link will only show if the user is logged in*/}
-        {auth.token ? (
-          <Link to="/" onClick={handleLogout}>
-            LOGOUT
-          </Link>
-        ) : (
-          <Link to="/login">LOGIN</Link>
-        )}
+    <>
+      <nav className="navbar">
+        {/* Logo Image */}
+        <img src={Image} alt="Logo" className="logo-image" />
+
+        {/* Links container */}
+        <div className="links">
+          <Link to="/">Home</Link>
+          {auth.token ? (
+            <Link to="/login" onClick={() => setAuth({})}>
+              LOGOUT
+            </Link>
+          ) : (
+            <Link to="/login">Login</Link>
+          )}
+        </div>
       </nav>
+
+      {/* This Outlet will render the HomePage, or any other nested routes */}
       <Outlet />
-    </div>
+    </>
   );
 }
 
