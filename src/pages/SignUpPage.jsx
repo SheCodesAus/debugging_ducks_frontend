6b679322491a9ee34abeeaaa4e68c7d16739b899
Bianca Di Biase <Bianca@Biancas-MacBook-Air.local>
2025-01-27 13:23:15 +0800

    added CSS to SignUpPage.css

--- src/pages/SignUpPage.jsx
+++ src/pages/SignUpPage.jsx
@@ -12,25 +12,36 @@ function SignUpPage() {
   if (auth?.token) {
     return (
       <div className="signup-page">
-        <h2>You are already logged in!</h2>
-        <p>
-          Log out if you wish to create a new account. Otherwise, explore the
-          site!
-        </p>
-        <button onClick={() => navigate("/")}>Go Home</button>
+        <div className="signup-container">
+          <h2>You are already logged in!</h2>
+          <p>
+            Log out if you wish to create a new account. Otherwise, explore the
+            site!
+          </p>
+          <button onClick={() => navigate("/")}>Go Home</button>
+        </div>
       </div>
     );
   }
 
   return (
     <div className="signup-page">
-      <h2>Sign Up</h2>
-      <p>Sign up to create a new account</p>
-      <SignUpForm />
-      <p>
-        Already have an account?{" "}
-        <button onClick={() => navigate("/login")}>Login here</button>
-      </p>
+      <div className="signup-container">
+        <h1 className="login-heading">
+          <span className="icon">꧁</span> Sign Up{" "}
+          <span className="icon flip-icon">꧁</span>
+        </h1>
+
+        <h2>Sign Up</h2>
+        <p>Sign up to create a new account</p>
+        <SignUpForm />
+        <div className="sign-up-link">
+          <p>
+            Already have an account?{" "}
+            <button onClick={() => navigate("/login")}>Login here</button>
+          </p>
+        </div>
+      </div>
     </div>
   );
 }
