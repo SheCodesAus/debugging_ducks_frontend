254d88d3508fb53c99478053bd09519d35fc673d
Bianca Di Biase <Bianca@Biancas-MacBook-Air.local>
2025-01-31 18:19:09 +0800

    made changes to NotFoundPage.css

--- src/pages/NotFoundPage.jsx
+++ src/pages/NotFoundPage.jsx
@@ -1,7 +1,7 @@
 // src/pages/NotFoundPage.jsx
-import React from 'react';
-import { Link } from 'react-router-dom';
-import './NotFoundPage.css';
+import React from "react";
+import { Link } from "react-router-dom";
+import "./NotFoundPage.css";
 
 function NotFoundPage() {
   return (
@@ -17,14 +17,20 @@ function NotFoundPage() {
         <div className="snowflake">❅</div>
         <div className="snowflake">❆</div>
       </div>
-      
+
       {/* Card Container */}
       <div className="not-found-card">
-        <h1>❄️ 404 - Page Not Found ❄️</h1>
-        <p> Oh no! It looks like this page didn’t make it onto The Good List. 
-            Don’t worry, you can find your way back to the homepage using the link below.
+        <h1>
+          <span className="icon">꧁</span>404 - Page Not Found{" "}
+          <span className="icon flip-icon">꧁</span>
+        </h1>
+        <p>
+          {" "}
+          Oh no! It looks like this page didn’t make it onto The Good List.
+          Don’t worry, you can find your way back to the homepage using the link
+          below.
         </p>
-        <Link to="/">Go back to Homepage</Link>
+        <Link to="/">Return to Homepage</Link>
       </div>
     </div>
   );
