6e9ce5894d90fc401c7df1298364dd92294645f8
Afira <afiratahmali@gmail.com>
2025-02-01 20:15:15 +0800

    imported main.css to main.jsx and removed font import in index.html

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import "./main.css";
 
 // Import pages (components for different views)
 import HomePage from "./pages/HomePage";
