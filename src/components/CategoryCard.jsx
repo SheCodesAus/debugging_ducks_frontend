7987fa52d91a67436bc2fbd39b653f51f5a1736e
Bianca Di Biase <Bianca@Biancas-MacBook-Air.local>
2025-02-01 15:39:30 +0800

    made changes to List Landing Page styling

--- src/components/CategoryCard.jsx
+++ src/components/CategoryCard.jsx
@@ -12,43 +12,43 @@ function CategoryCard({ category, lists, onCreateList }) {
           src={LeftVector}
           alt="left flourish image"
         />
-        <h3> {category.category_name} </h3>
+        <h3>{category.category_name}</h3>
         <img
           className="flourish-design"
           src={RightVector}
-          alt="right flourish image" 
+          alt="right flourish image"
         />
       </div>
 
-      <p>Budget: ${category.category_budget}</p>
+      {/* <p>Budget: ${category.category_budget}</p> */}
       <div className="category-lists">
-                {lists.map(list => (
-                    <div 
-                        key={list.id} 
-                        className="list-header"
-                        onClick={() => onListClick(list.id)}
-                    >
-                                <h4>{list.list_name}</h4>
-                                <p>Budget: ${list.individual_budget}</p>
-                                {list.notes && <p className="list-notes">{list.notes}</p>}
-                            </div>
-                ))}
-                <button 
-                    onClick={() => onCreateList(category.id, category.category_name)}
-                    className="create-list-button"
-                >
-                    Create List
-                </button>
-            </div>
+        {lists.map((list) => (
+          <div
+            key={list.id}
+            className="list-header"
+            onClick={() => onListClick(list.id)}
+          >
+            <h4>{list.list_name}</h4>
+            {/* <p>Budget: ${list.individual_budget}</p> */}
+            {list.notes && <p className="list-notes">{list.notes}</p>}
+          </div>
+        ))}
+        <button
+          onClick={() => onCreateList(category.id, category.category_name)}
+          className="create-list-button"
+        >
+          + Add New List
+        </button>
+      </div>
     </div>
   );
 }
 
 CategoryCard.propTypes = {
-    category: PropTypes.object.isRequired,
-    lists: PropTypes.array.isRequired,
-    onCreateList: PropTypes.func.isRequired,
-    onListClick: PropTypes.func.isRequired
+  category: PropTypes.object.isRequired,
+  lists: PropTypes.array.isRequired,
+  onCreateList: PropTypes.func.isRequired,
+  onListClick: PropTypes.func.isRequired,
 };
 
 export default CategoryCard;
