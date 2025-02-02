8ad4b4c7825d16bc1bd7207445a59aec866f6434
Bianca Di Biase <Bianca@Biancas-MacBook-Air.local>
2025-02-02 21:39:48 +0800

    added a Snowflakes.jsx and Snowflakes.css file

---
+++ src/components/Snowflakes.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+import "./Snowflakes.css";
+
+const Snowflakes = () => {
+  return (
+    <div className="snowflakes" aria-hidden="true">
+      <div className="snowflake left">❅</div>
+      <div className="snowflake right">❆</div>
+      <div className="snowflake left">❄</div>
+      <div className="snowflake right">❅</div>
+      <div className="snowflake left">❆</div>
+      <div className="snowflake right">❄</div>
+      <div className="snowflake left">❅</div>
+      <div className="snowflake right">❆</div>
+    </div>
+  );
+};
+
+export default Snowflakes;
