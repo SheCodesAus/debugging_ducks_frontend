90b3b39594c9692e1f9ab9d13d0ceecf20a868da
Afira <afiratahmali@gmail.com>
2025-02-03 12:49:28 +0800

    fixed item header and component grids and major update to the item grid and item card

--- src/pages/ListDetailsPage.jsx
+++ src/pages/ListDetailsPage.jsx
@@ -220,10 +220,8 @@ function ListDetailsPage() {
               <div className="header-item">Item</div>
               <div className="header-cost">Cost</div>
               <div className="header-favorite">♥</div>
-              <div className="header-purchased">
-                🎁
-                <p className="header-delete"></p>
-              </div>
+              <div className="header-purchased">🎁</div>
+              <div className="header-delete">x</div>
             </div>
             {items.map((item) => (
               <ItemCard
